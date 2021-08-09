be67eacaef1ea94f8890aa4a3a22015c22ba566c
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-08-09 12:54:08 -0500

    the grid works

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const tileCon = document.querySelector('.tileCon')
+let gameGrid = []
 
+for (let i = 0; i < 18; i++) {
+  gameGrid.push({ column: i, row: [] })
+  for (let j = 0; j < 32; j++) {
+    gameGrid[i].row.push(j)
+    let tile = document.createElement('div')
+    tile.className = `${i}-${j}`
+    tile.innerText = `X`
+    tile.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+    tile.style.width = '40px'
+    tile.style.height = '40px'
+    tile.style.gridArea = `${i + 1} / ${j + 1} / ${i + 2} / ${j + 2}`
+    tileCon.append(tile)
+  }
+}
+console.log(gameGrid)
